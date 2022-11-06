ca42b2ecde306a4d2892e0f93ef2d092971a4be6
vgonzalescabanillas <vgonzalescabanillas@gmail.com>
2022-11-06 17:31:00 -0500

    trabajo viernes 2

---
+++ Trabajo/build/paises.mjs
@@ -0,0 +1,28 @@
+"use strict";
+import fetch from 'node-fetch'
+(() => {
+    const cod = {
+        codpais: "pe",
+    };
+    const searchByCodeCountry = async (cod) => {
+        try {
+            const res = await fetch(`https://restcountries.com/v3.1/alpha/${cod}`);
+            const data = await res.json();
+            const respuesta = {
+                nombre: data[0].name.common,
+                monedanom: data[0].currencies[Object.keys(data[0].currencies)].name,
+                monedasimbolo: data[0].currencies[Object.keys(data[0].currencies)].symbol,
+                idioma: ""
+            };
+            Object.entries(data[0].languages).find(([key, value]) => {
+                respuesta.idioma = respuesta.idioma + value + " ";
+                return false;
+            });
+            return console.log(`El pais ingrasado es : ${respuesta.nombre} , su moneda es : ${respuesta.monedanom} y su simbolo es : ${respuesta.monedasimbolo}, los idioma(s) : ${respuesta.idioma}`);
+        }
+        catch (error) {
+            console.log(error);
+        }
+    };
+    searchByCodeCountry(cod.codpais);
+})();
--- Trabajo/paises.ts
+++ Trabajo/paises.ts
@@ -1,11 +1,11 @@
-
 (()=>{
   interface cod {
     codpais : string,
-
+ 
   }
   const cod : cod ={
-    codpais : "pe"
+    codpais : "pe",
+ 
   }
 
   const searchByCodeCountry = async (cod:string) => {
@@ -13,11 +13,21 @@
     try {
       const res = await fetch(
         `https://restcountries.com/v3.1/alpha/${cod}`
-      )/* .then(res=>res.json())      
-        .then((data)=>console.log(data)) */
+      )
         
-      const data = await res.json();
-       return data;
+      const data = await res.json();    
+        const respuesta = {
+          nombre : data[0].name.common,
+          monedanom : data[0].currencies[Object.keys(data[0].currencies)].name,     
+          monedasimbolo :   data[0].currencies[Object.keys(data[0].currencies)].symbol,                      
+          idioma: ""
+        };
+        Object.entries(data[0].languages).find(([key, value]) => {
+            respuesta.idioma = respuesta.idioma + value + " ";
+          return false;
+        });
+        return console.log(`El pais ingrasado es : ${respuesta.nombre} , su moneda es : ${respuesta.monedanom} y su simbolo es : ${respuesta.monedasimbolo}, los idioma(s) : ${respuesta.idioma}`);  
+       
       
     } catch (error) {
       console.log(error);
